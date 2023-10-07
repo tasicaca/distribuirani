4e91b97bc4fee1ad657c1214fc1dadb4f94cb315
Aleksandar T <96747833+aleksandartasic@users.noreply.github.com>
2023-10-07 03:01:21 +0200

    Update oktobarII23.cpp

--- oktobarII23.cpp
+++ oktobarII23.cpp
@@ -7,13 +7,13 @@ int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 
 	int rank, size;
-	int datalocalno = 0;
+	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+	int datalocalno = rank;
 	int data = rank; // Example data to be exchanged
 	int partner_data;
-	double partner_rank;
+	int partner_rank;
 
 	for (int i = 0; i < log2(size); i++) {
 			partner_rank = rank ^ (1 << i);
@@ -24,10 +24,11 @@ int main(int argc, char** argv) {
 
 			printf("Process %d exchanges data with process %d: My data: %d, Partner's data: %d\n", rank, partner_rank, data, partner_data);
 			data += partner_data;
-			// You can update the value of "data" here based on the partner's data if necessary
-			if (partner_rank < rank)
+		
+			if (partner_rank <= rank)
 			{
 				datalocalno += partner_data;
+				 
 				printf("rank %d  data: %d    \n", rank, datalocalno);
 			}
 			
